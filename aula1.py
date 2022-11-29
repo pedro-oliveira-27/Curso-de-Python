442766d4fc5c11489bae0256d49a2aad10ca22ce
pedro-oliveira-27 <118461389+pedro-oliveira-27@users.noreply.github.com>
2022-11-28 22:10:52 -0300

    Create aula1.py

---
+++ aula1.py
@@ -0,0 +1,13 @@
+""" 
+docstring (várias linhas)
+diferente de comentário, o python faz a leitura 
+
+"""
+
+# permite escrever um comentário 
+print(123) # print imprime o que tem do parenteses 
+
+print("olá mundo!!")
+
+
+
